93c4785dbcce0dbac751facd03d4f8163d88c705
felipeatari <mr.robot.felipe@gmail.com>
2023-12-05 10:08:24 -0300

    Ignora os arquivos minificados 4

--- storage/minify/main.js
+++ storage/minify/main.js
@@ -1,1 +1,1 @@
-'use strict';const url = 'http://localhost:8888';
+'use strict';const url = 'http://localhost:8888';let item = document.querySelector('.slider-item');let inicia_slide = 1;let limite_slides = 3;item.src = `${url}/storage/theme/banner-${inicia_slide}.jpg`;function voltar() {inicia_slide--;if (inicia_slide < 1) {inicia_slide = limite_slides;}item.src = `${url}/storage/theme/banner-${inicia_slide}.jpg`;}function avancar() {inicia_slide++;if (inicia_slide > limite_slides) {inicia_slide = 1;}item.src = `${url}/storage/theme/banner-${inicia_slide}.jpg`;}setInterval(avancar, 5000);
