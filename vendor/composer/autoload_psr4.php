b86727be68029aed9b79fa3de4f3a4c56cd88765
felipeatari <62570353+felipebraavo1@users.noreply.github.com>
2024-01-19 18:45:47 -0300

    Todas as classes na Src

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,5 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Src\\' => array($baseDir . '/source'),
+    'Src\\' => array($baseDir . '/Src'),
 );
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '33df3a07f6d5c712b0c5257b8bf27b50da951e45',
+        'reference' => '9cd704588ad8b0837f276c2c996eb39681eeb978',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '33df3a07f6d5c712b0c5257b8bf27b50da951e45',
+            'reference' => '9cd704588ad8b0837f276c2c996eb39681eeb978',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
