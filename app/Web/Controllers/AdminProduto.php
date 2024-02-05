fd722a435966a5dd78779235bdebb8b736fc363f
felipeatari <62570353+felipebraavo1@users.noreply.github.com>
2024-02-05 04:03:39 -0300

    Fix: Valida se tem categorias para exibir

--- app/Web/Controllers/AdminProduto.php
+++ app/Web/Controllers/AdminProduto.php
@@ -17,6 +17,9 @@ class AdminProduto extends Admin
     $content = [];
 
     if (isset($_GET['action']) and $_GET['action'] === 'criar') {
+      pr($_POST);
+      pr($_GET);
+      die;
       $this->categoria_criar($_POST);
 
       Router::redirect('/admin/produto/categorias');
